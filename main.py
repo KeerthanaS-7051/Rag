e543e43db36d1cd54598dd501d6db505521ada52
KeerthanaS-7051 <keerthana7051@gmail.com>
2025-07-29 15:16:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from mcp_servers.server import EmployeeChatbotServer
 
 def main():
     server = EmployeeChatbotServer()
-    print("🚀 Employee Chatbot MCP Server running on HTTP...")
+    print("Employee Chatbot MCP Server running on HTTP...")
     uvicorn.run(server.app, host="127.0.0.1", port=8000)
 
 if __name__ == "__main__":
